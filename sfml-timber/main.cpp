010dc3d86793dc7c9a1f922b9c5fbf3bd1dc1d70
mitsiu <mitsiu.carreno@gmail.com>
2018-12-05 07:40:00 -0600

    sfml default proyect

--- sfml-timber/main.cpp
+++ sfml-timber/main.cpp
@@ -1,3 +1,30 @@
+/*
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200,200), "Hello friend");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
+    return 0;
+}
+*/
 
 //
 // Disclaimer:
@@ -89,3 +116,4 @@ int main(int, char const**)
 
     return EXIT_SUCCESS;
 }
+
