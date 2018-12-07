b919414136d913bf985334bb5cfd67d6a90b128f
mitsiu-carreno <mitsiu.carreno@gmail.com>
2018-12-07 17:56:09 -0600

    start sfml from book

---
+++ first_window.cpp
@@ -0,0 +1,124 @@
+/*
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200,200), "Hello friend");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
+    return 0;
+}
+*/
+
+//
+// Disclaimer:
+// ----------
+//
+// This code will work only if you selected window, graphics and audio.
+//
+// Note that the "Run Script" build phase will copy the required frameworks
+// or dylibs to your application bundle so you can execute it on any OS X
+// computer.
+//
+// Your resource files (images, sounds, fonts, ...) are also copied to your
+// application bundle. To get the path to these resources, use the helper
+// function `resourcePath()` from ResourcePath.hpp
+//
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+// Here is a small helper for you! Have a look.
+#include "ResourcePath.hpp"
+
+int main(int, char const**)
+{
+
+
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    // Set the Icon
+    sf::Image icon;
+    if (!icon.loadFromFile(resourcePath + "icon.png")) {
+    //if(!icon.loadFromFile("/Users/mitsiu/Documents/dev_hell/sfml-timber/src/icon.png")){
+        return EXIT_FAILURE;
+    }
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile(resourcePath + "cute_image.jpg")) {
+    //if(!texture.loadFromFile("/Users/mitsiu/Documents/dev_hell/sfml-timber/src/cute_image.jpg")){
+        return EXIT_FAILURE;
+    }
+    sf::Sprite sprite(texture);
+
+    // Create a graphical text to display
+    sf::Font font;
+    if (!font.loadFromFile(resourcePath + "sansation.ttf")) {
+    //if(!font.loadFromFile("/Users/mitsiu/Documents/dev_hell/sfml-timber/src/sansation.ttf")){
+        return EXIT_FAILURE;
+    }
+    sf::Text text("Hello SFML", font, 50);
+    text.setFillColor(sf::Color::Black);
+
+    // Load a music to play
+    sf::Music music;
+    if (!music.openFromFile(resourcePath + "nice_music.ogg")) {
+    //if(!music.openFromFile("/Users/mitsiu/Documents/dev_hell/sfml-timber/src/nice_music.ogg")){ 
+       return EXIT_FAILURE;
+    }
+
+    // Play the music
+    music.play();
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            // Escape pressed: exit
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,124 +1,26 @@
-/*
+// Include important C++ libraries here
+// #include "stdafx.h"
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200,200), "Hello friend");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    
-    while(window.isOpen())
-    {
-        sf::Event event;
-        while(window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-    
-    return 0;
-}
-*/
+  // Create a video mode object
+  VideoMode video(1920,1080);
 
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
+  // Create and open a window for the game RenderWindow
+  RenderWindow window(video, "Timber!!!");
 
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
-
-// Here is a small helper for you! Have a look.
-#include "ResourcePath.hpp"
-
-int main(int, char const**)
+  // Display the list of all the video modes available for fullscreen
+std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+for (std::size_t i = 0; i < modes.size(); ++i)
 {
-
-
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath + "icon.png")) {
-    //if(!icon.loadFromFile("/Users/mitsiu/Documents/dev_hell/sfml-timber/src/icon.png")){
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath + "cute_image.jpg")) {
-    //if(!texture.loadFromFile("/Users/mitsiu/Documents/dev_hell/sfml-timber/src/cute_image.jpg")){
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath + "sansation.ttf")) {
-    //if(!font.loadFromFile("/Users/mitsiu/Documents/dev_hell/sfml-timber/src/sansation.ttf")){
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath + "nice_music.ogg")) {
-    //if(!music.openFromFile("/Users/mitsiu/Documents/dev_hell/sfml-timber/src/nice_music.ogg")){ 
-       return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-        }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
-    }
-
-    return EXIT_SUCCESS;
+    sf::VideoMode mode = modes[i];
+    std::cout << "Mode #" << i << ": "
+              << mode.width << "x" << mode.height << " - "
+              << mode.bitsPerPixel << " bpp" << std::endl;
+}
+  return 0;
 }
