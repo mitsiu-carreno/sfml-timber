b3b148f28991382104f92d84ccd6a343f3919e44
mitsiu <mitsiu.carreno@gmail.com>
2018-12-09 16:05:30 -0600

    set background on window

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,26 @@ int main()
 
   // Create a video mode object
   VideoMode video(1280,800);
+  //VideoMode video(640, 480);
 
   // Create and open a window for the game RenderWindow
   RenderWindow window(video, "Timber!!!", Style::Fullscreen);
   
+  // Create a texture to hold a graphic on the CPU
+  Texture textureBackground;
+
+  // Load a graphic into the texture
+  textureBackground.loadFromFile("resources/graphics/background.png");
+
+  // Create a sprite
+  Sprite spriteBackground;
+
+  // Attach a texture to the sprite
+  spriteBackground.setTexture(textureBackground);
+
+  // Set the spriteBackground to cover the screen
+  spriteBackground.setPosition(0,0);
+
   while(window.isOpen())
   {
     /******************************
@@ -48,6 +64,7 @@ int main()
     window.clear();
 
     // Draw our game scene here
+    window.draw(spriteBackground);
 
     // Show everything we just drew
     window.display();
