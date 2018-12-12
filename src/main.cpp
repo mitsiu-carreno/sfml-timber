607d2803249c50d26e491069ba63392681a71752
mitsiu <mitsiu.carreno@gmail.com>
2018-12-12 00:12:33 -0600

    start playing around with scales and sizes to fit images to screen

--- src/main.cpp
+++ src/main.cpp
@@ -20,22 +20,37 @@ int main()
 
   // Load a graphic into the texture
   textureBackground.loadFromFile("resources/graphics/background.png");
-
   // Create a sprite
   Sprite spriteBackground;
-
   // Attach a texture to the sprite
   spriteBackground.setTexture(textureBackground);
-
   // Set the spriteBackground to cover the screen
   spriteBackground.setPosition(0,0);
+  // Scale to fit window 
+  spriteBackground.setScale(
+    1280/spriteBackground.getLocalBounds().width, 
+    800/spriteBackground.getLocalBounds().height);
 
   // Make a tree sprite 
   Texture textureTree;
   textureTree.loadFromFile("resources/graphics/tree.png");
   Sprite spriteTree;
   spriteTree.setTexture(textureTree);
-  spriteTree.setPosition(600,0);
+  // Center tree (half window - half sprite)
+  spriteTree.setPosition(
+    (1280/2)-spriteTree.getGlobalBounds().width/2,
+    0);
+
+  // Prepare the bee
+  Texture textureBee;
+  textureBee.loadFromFile("resources/graphics/bee.png");
+  Sprite spriteBee;
+  spriteBee.setTexture(textureBee);
+  spriteBee.setPosition(0,700);
+  // Is the bee currently movinb
+  //bool beeActive = false;
+  // How fast can the bee fly?
+  //float beeSpeed = 0.0f;
 
   while(window.isOpen())
   {
