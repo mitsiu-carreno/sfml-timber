7a8e76b2439276b6597406ba049056133cc2107c
mitsiu <mitsiu.carreno@gmail.com>
2018-12-12 22:58:05 -0600

    scale and position sprites at will (f*ck yeah!)

--- src/main.cpp
+++ src/main.cpp
@@ -24,36 +24,45 @@ int main()
   
   // Create a texture to hold a graphic on the CPU
   Texture textureBackground;
-
   // Load a graphic into the texture
   textureBackground.loadFromFile("resources/graphics/background.png");
   // Create a sprite
   Sprite spriteBackground;
   // Attach a texture to the sprite
   spriteBackground.setTexture(textureBackground);
-  // Set the spriteBackground to cover the screen
-  spriteBackground.setPosition(0,0);
   // Scale to fit window 
   spriteBackground.setScale(
     windowSize.width/spriteBackground.getLocalBounds().width, 
     windowSize.height/spriteBackground.getLocalBounds().height);
+  // Set the spriteBackground to cover the screen
+  spriteBackground.setPosition(0,0);
+  
 
   // Make a tree sprite 
   Texture textureTree;
   textureTree.loadFromFile("resources/graphics/tree.png");
   Sprite spriteTree;
   spriteTree.setTexture(textureTree);
+  // Scale tree to cover 1/5 of the window width
+  spriteTree.setScale(
+    (windowSize.width/5)/spriteTree.getLocalBounds().width,
+    1);
   // Center tree (half window - half sprite)
   spriteTree.setPosition(
     (windowSize.width/2)-spriteTree.getGlobalBounds().width/2,
     0);
+  
 
   // Prepare the bee
   Texture textureBee;
   textureBee.loadFromFile("resources/graphics/bee.png");
   Sprite spriteBee;
   spriteBee.setTexture(textureBee);
-  spriteBee.setPosition(0,700);
+  spriteBee.setPosition(windowSize.width/4,windowSize.height/5);
+  spriteBee.setScale(
+    (windowSize.width/20)/spriteBee.getLocalBounds().width,
+    (windowSize.width/20)/spriteBee.getLocalBounds().width
+  );
   // Is the bee currently movinb
   //bool beeActive = false;
   // How fast can the bee fly?
@@ -95,6 +104,7 @@ int main()
     // Draw our game scene here
     window.draw(spriteBackground);
     window.draw(spriteTree);
+    window.draw(spriteBee);
 
     // Show everything we just drew
     window.display();
