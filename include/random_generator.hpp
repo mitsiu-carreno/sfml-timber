dfde51cd431103ae68f6be257df2a5a4a993cce1
mitsiu <mitsiu.carreno@gmail.com>
2018-12-13 07:09:06 -0600

    try and add a random generator, work in progress

---
+++ include/random_generator.hpp
@@ -0,0 +1,6 @@
+#ifndef RANDOM_GENERATOR_H
+#define RANDOM_GENERATOR_H
+
+int gen_random(int min, int max);
+
+#endif
---
+++ src/random_generator.cpp
@@ -0,0 +1,17 @@
+#include <random>
+#include <ctime>
+
+int gen_random(int min, int max, int totalNumbers)
+{
+    // Initialize our mersenne twister with a random seed based on the clock
+    std::mt19937 mersenne(static_cast<unsigned int>(std::time(nullptr)));
+
+    // Create a reusable random number generator that generates uniform numbers between min and max
+    std::uniform_int_distribution<> dice(min, max);
+
+    //int *randoms = new int[totalNumbers];
+    for(int count=1; count <= totalNumbers; ++count)
+    {
+
+    }
+}
