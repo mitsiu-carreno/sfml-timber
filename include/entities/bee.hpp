3a86c0db1426f94492d80ed9b1174392d550f171
mitsiu-carreno <mitsiu.carreno@gmail.com>
2019-02-06 19:17:39 -0600

    try to smooth bee movement FAILED

--- include/entities/bee.hpp
+++ include/entities/bee.hpp
@@ -12,14 +12,14 @@ struct Bee
 {
   sf::Sprite spriteBee;
   Point origin;
-  int radius = 0;
+  int radius = 100;
   float angle = 0.0;
   float secondsInPath = 0.0f;
   float elapsedTimeInPath = 0.0f;
   bool clockWise = true;
 
   bool inPath = false;
-  int speed = 2;    // measured in degrees/second 
+  float speed = 0.9;    // measured in degrees/second 
 };
 
 
--- src/move_bee.cpp
+++ src/move_bee.cpp
@@ -17,7 +17,45 @@ void setBeePosition(Bee &bee, sf::Time dt)
 {
   // Set a new circle center and seconds to spend in path
   if(!bee.inPath)
-  {
+  { 
+    bee.spriteBee.setPosition(
+        getXCircunference(bee),
+        getYCircunference(bee)
+    );
+    std::cout << "origin: " << bee.origin.x << "," << bee.origin.y << " radius: " << bee.radius << " angle : " << bee.angle << " calc bee position: " << getXCircunference(bee) << "," << getYCircunference(bee) << " real bee position: " << bee.spriteBee.getPosition().x << "," << bee.spriteBee.getPosition().y << std::endl;
+    
+    // New circle center is at current bee position (temporarly)
+    bee.origin.x = getXCircunference(bee);
+    bee.origin.y = getYCircunference(bee);
+    // ----Fix later to limit on screen dimentions
+    bee.radius = 200;
+    //bee.radius = gen_random(200, 500);
+    std::cout << "new rad " << bee.radius << std::endl; 
+    // New circle center is at current bee position (temporarly)
+    bee.origin.x = getXCircunference(bee);
+    bee.origin.y = getYCircunference(bee);
+    // Real circle center is calc 
+
+    //Invert angle 
+    //std::cout << "old angle " << bee.angle << std::endl;
+    bee.angle = bee.angle + 180;
+    std::cout << "new angle " << bee.angle << std::endl;
+    
+    
+    
+    //Modify slightly angle to produce non-perfect waves
+    //bee.angle += gen_random(-10,10);
+
+    
+    
+    // Toggle direction to spin 
+    bee.clockWise = !bee.clockWise;
+    bee.secondsInPath = 2;
+    //bee.secondsInPath = gen_random(2,4);
+    bee.inPath = true;
+
+
+    /*
     bee.origin.x = gen_random(10,getWindowWidth()-10);
     // y origin go from 25% to 75% of screen height
     bee.origin.y = gen_random(getWindowHeight()/4,getWindowHeight()-getWindowHeight()/4);
@@ -29,8 +67,9 @@ void setBeePosition(Bee &bee, sf::Time dt)
     // Toggle direction to spin 
     bee.clockWise = !bee.clockWise;
     bee.inPath = true;
+    */
+    std::cout << "origin: " << bee.origin.x << "," << bee.origin.y << " radius: " << bee.radius << " angle : " << bee.angle << " calc bee position: " << getXCircunference(bee) << "," << getYCircunference(bee) << " real bee position: " << bee.spriteBee.getPosition().x << "," << bee.spriteBee.getPosition().y << "\n\n\n";
 
-    std::cout << "origin: " << bee.origin.x << "," << bee.origin.y << " radius: " << bee.radius << " angle : " << bee.angle << " secondsInPath: " << bee.secondsInPath  << std::endl;
 
   } 
   else
@@ -45,13 +84,20 @@ void setBeePosition(Bee &bee, sf::Time dt)
       ); 
 
       if(bee.clockWise){
-        bee.angle += dt.asSeconds() * bee.speed;
+        //bee.angle += dt.asSeconds() * bee.speed;
+        bee.angle += 0.01;
       }else{
-        bee.angle -= dt.asSeconds() * bee.speed;
+        //bee.angle -= dt.asSeconds() * bee.speed;
+        bee.angle -= 0.01;
       }
       
 
     }else{
+      bee.spriteBee.setPosition(
+        getXCircunference(bee),
+        getYCircunference(bee)
+    );
+      std::cout<< "last calc bee position: " << getXCircunference(bee) << "," << getYCircunference(bee) << " real bee position: " << bee.spriteBee.getPosition().x << "," << bee.spriteBee.getPosition().y << "\n\n\n";
       bee.elapsedTimeInPath = 0.0f;
       bee.inPath = false;
     }
